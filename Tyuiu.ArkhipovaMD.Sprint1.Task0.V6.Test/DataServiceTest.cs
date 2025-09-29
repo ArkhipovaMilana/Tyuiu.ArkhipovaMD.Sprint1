69269b65d76b02ebf0ed16958e965410a4a4974c
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-09-29 14:08:55 +0500

    Add project files.

---
+++ Tyuiu.ArkhipovaMD.Sprint1.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.ArkhipovaMD.Sprint1.Task0.V6.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint1.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            var res = ds.Calculate();
+
+            Assert.AreEqual(15, res);
+        }
+    }
+}
---
+++ Tyuiu.ArkhipovaMD.Sprint1.Task0.V6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ArkhipovaMD.Sprint1.Task0.V6.Lib;
+
+namespace Tyuiu.ArkhipovaMD.Sprint1.Task0.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Архипова М.Д. | АСОиУб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнила: Архипова М.Д. | АСОиУб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 3*(4+1)   и печатает    *");
+            Console.WriteLine("* результат на экране.                                                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 3*(4+1)                                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
