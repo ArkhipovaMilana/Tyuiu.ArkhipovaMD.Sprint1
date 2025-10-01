0672cbc68ff576d600421976a5d6d416eb7f2657
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-01 11:57:21 +0500

    Task4

---
+++ Tyuiu.ArkhipovaMD.Sprint1.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint1.Task4.V2.Lib;
+namespace Tyuiu.ArkhipovaMD.Sprint1.Task4.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            var res = ds.Calculate(0, 2);
+            Assert.AreEqual(0.5, res);
+        }
+    }
+}
---
+++ Tyuiu.ArkhipovaMD.Sprint1.Task4.V2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint1.Task4.V2.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #1 | Выполнила: Архипова М.Д. | АСОиУб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #2                                                              *");
+Console.WriteLine("* Выполнила: Архипова М.Д. | АСОиУб-25-1                                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, вычисляет результат по формуле и печатает его на экране. Ответ округлите до 3 знаков после запятой.            *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+double x;
+Console.WriteLine("Введите значение X");
+x = Convert.ToInt32(Console.ReadLine());
+double y;
+Console.WriteLine("Введите значение Y");
+y = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("РЕЗУЛЬТАТ:                                                                *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.Calculate(x,y));
