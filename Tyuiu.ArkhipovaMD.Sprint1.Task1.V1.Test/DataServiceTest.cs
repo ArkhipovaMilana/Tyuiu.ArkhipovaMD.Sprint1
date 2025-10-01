e276439b293e0c397cdc740ebc0224cc84f36dfc
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-01 09:59:01 +0500

    Task1

---
+++ Tyuiu.ArkhipovaMD.Sprint1.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint1.Task1.V1.Lib;
+namespace Tyuiu.ArkhipovaMD.Sprint1.Task1.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double k = 1;
+            double i =1;
+            double j = 3;
+            var res = ds.Calculate(i,j,k);
+            Assert.AreEqual(7, res);
+        }
+    }
+}
---
+++ Tyuiu.ArkhipovaMD.Sprint1.Task1.V1/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint1.Task1.V1.Lib;
+namespace Tyuiu.ArkhipovaMD.Sprint1.Task1.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Архипова М.Д. | АСОиУб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнила: Архипова М.Д. | АСОиУб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, вычисляет результат по формуле x/3/y+6*a и печатает его на экране.            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int a;
+            Console.WriteLine("Введите значение A:");
+            a = Convert.ToInt32(Console.ReadLine());
+            int x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+            int y;
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine( ds.Calculate(a,x,y));
+
+        }
+    }
+}
