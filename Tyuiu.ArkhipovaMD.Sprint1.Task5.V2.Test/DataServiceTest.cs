2ccbc02ff635572fcd19d3718a0d1f81c2b85fc3
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-02 22:59:18 +0500

    Task5

---
+++ Tyuiu.ArkhipovaMD.Sprint1.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint1.Task5.V2.Lib;
+namespace Tyuiu.ArkhipovaMD.Sprint1.Task5.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = ds.FahrenheitToСelsius(32);
+            Assert.AreEqual(0, x);
+        }
+    }
+}
---
+++ Tyuiu.ArkhipovaMD.Sprint1.Task5.V2/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.ArkhipovaMD.Sprint1.Task5.V2.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #1 | Выполнила: Архипова М.Д. | АСОиУб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #2                                                              *");
+Console.WriteLine("* Выполнила: Архипова М.Д. | АСОиУб-25-1                                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дано значение температуры в градусах Фаренгейта. Определить значение этой же температуры в градусах Цельсия. Ответ привести к целому с помощью класса Convert.            *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+double x;
+Console.WriteLine("Введите значение X");
+x = Convert.ToDouble(Console.ReadLine());
+
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("РЕЗУЛЬТАТ:                                                                *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.FahrenheitToСelsius(x));
